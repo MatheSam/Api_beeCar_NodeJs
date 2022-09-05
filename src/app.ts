4fb8da823c8c0194fdc8f3b909ce41436594f360
Matheus Sam <matheus.snavarro@outlook.com>
2022-09-05 17:01:53 -0300

    feat/created routes

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,28 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 import { handleError } from "./middlewares/errors.mid";
+import { addressRouter } from "./routes/address";
+import { cardRouter } from "./routes/card";
+import { carsRouter } from "./routes/cars";
+import { categoryRouter } from "./routes/category";
+import { cnhRouter } from "./routes/cnh";
+import { loginRouter } from "./routes/login";
+import { maintenenceRouter } from "./routes/maintenence";
+import { rentRouter } from "./routes/rent";
+import { usersRouter } from "./routes/users";
 
 const app = express();
 app.use(express.json());
+
+app.use("/address", addressRouter);
+app.use("/card", cardRouter);
+app.use("/cars", carsRouter);
+app.use("/category", categoryRouter);
+app.use("/cnh", cnhRouter);
+app.use("/login", loginRouter);
+app.use("/maintenence", maintenenceRouter);
+app.use("/rent", rentRouter);
+app.use("/users", usersRouter);
 
 app.use(handleError);
 
---
+++ src/routes/category/index.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+export const categoryRouter = Router();
+
+categoryRouter.post("");
+categoryRouter.get("");
+categoryRouter.patch("");
+categoryRouter.delete("");
