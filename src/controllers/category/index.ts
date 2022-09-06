dd74ccd8799f1097a5d94b6078f4185d13982156
unknown <juliocomp99@hotmail.com>
2022-09-06 15:37:26 -0300

    feat [category]: list categories

--- src/controllers/category/index.ts
+++ src/controllers/category/index.ts
@@ -1,6 +1,7 @@
 import { AppError } from "../../errors/AppError";
 import { Request, Response } from "express";
-import createCategoryService from "../../services/category";
+import createCategoryService from "../../services/category/createCategory.service";
+import listCategoriesService from "../../services/category/listCategory.service";
 
 
 export const createCategoryController = async (req: Request, res: Response) => {
@@ -16,3 +17,18 @@ export const createCategoryController = async (req: Request, res: Response) => {
       }
     }
   };
+
+
+  export const listCategoryController = async (req: Request, res: Response) => {
+    try {
+        const category = await listCategoriesService();
+        return res.status(201).json(category);
+      } catch (error) {
+        if (error instanceof AppError) {
+          return res.status(error.statusCode).send({
+            message: error.message,
+          });
+        }
+      }
+    };
+  
