7d78d3dba3c92dafd801d12ca2702cfcf3128086
matheuszeiser <matheuszaizer@hotmail.com>
2022-09-06 14:05:01 -0300

    feat: Created list Cars route

---
+++ src/controllers/cars/listCars.controller.ts
@@ -0,0 +1,17 @@
+import { Response } from "express";
+import { AppError } from "../../errors/AppError";
+import { handleError } from "../../middlewares/errors.mid";
+import listCarsService from "../../services/cars/listCars.service";
+
+const listCarsController = async (req: Request, res: Response) => {
+  try {
+    const listCars = await listCarsService();
+    return res.json(listCars);
+  } catch (error) {
+    if (error instanceof AppError) {
+      handleError(error, res);
+    }
+  }
+};
+
+export default listCarsController;
