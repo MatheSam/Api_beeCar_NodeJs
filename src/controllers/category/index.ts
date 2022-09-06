8006c9ad809af5b2161716bc913a70b8c6de3a9c
Matheus Sam <matheus.snavarro@outlook.com>
2022-09-06 16:55:31 -0300

    fix/fixedBugs

--- src/controllers/category/index.ts
+++ src/controllers/category/index.ts
@@ -3,32 +3,29 @@ import { Request, Response } from "express";
 import createCategoryService from "../../services/category/createCategory.service";
 import listCategoriesService from "../../services/category/listCategory.service";
 
-
 export const createCategoryController = async (req: Request, res: Response) => {
-    try {
-      const data = req.body;
-      const category = await createCategoryService(data);
-      return res.status(201).json(category);
-    } catch (error) {
-      if (error instanceof AppError) {
-        return res.status(error.statusCode).send({
-          message: error.message,
-        });
-      }
+  try {
+    const data = req.body;
+    const category = await createCategoryService(data);
+    return res.status(201).json(category);
+  } catch (error) {
+    if (error instanceof AppError) {
+      return res.status(error.statusCode).send({
+        message: error.message,
+      });
     }
-  };
+  }
+};
 
-
-  export const listCategoryController = async (req: Request, res: Response) => {
-    try {
-        const category = await listCategoriesService();
-        return res.status(201).json(category);
-      } catch (error) {
-        if (error instanceof AppError) {
-          return res.status(error.statusCode).send({
-            message: error.message,
-          });
-        }
-      }
-    };
-  
+export const listCategoryController = async (req: Request, res: Response) => {
+  try {
+    const category = await listCategoriesService();
+    return res.status(201).json(category);
+  } catch (error) {
+    if (error instanceof AppError) {
+      return res.status(error.statusCode).send({
+        message: error.message,
+      });
+    }
+  }
+};
