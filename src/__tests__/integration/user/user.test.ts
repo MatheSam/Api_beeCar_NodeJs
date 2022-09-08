f6166321f9721c41fb3023ddc5ac36d3c00d09f3
Matheus Sam <matheus.snavarro@outlook.com>
2022-09-08 12:19:10 -0300

    feat/created user tests

--- src/__tests__/integration/user/user.test.ts
+++ src/__tests__/integration/user/user.test.ts
@@ -3,7 +3,12 @@ import AppDataSource from "../../../data-source";
 import request from "supertest";
 import app from "../../../app";
 import { describe, expect, test, beforeAll, afterAll } from "@jest/globals";
-import { mockedUser } from "../../mocks";
+import {
+  mockedLoginAdm,
+  mockedLoginUser,
+  mockedUser,
+  mockedUserAdm,
+} from "../../mocks";
 
 describe("/profile", () => {
   let connection: DataSource;
@@ -27,6 +32,60 @@ describe("/profile", () => {
   test("POST /profile - Must be able to create a user", async () => {
     const response = await request(app).post("/profile").send(mockedUser);
 
-    expect(response.body);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("name");
+    expect(response.body).toHaveProperty("birthDate");
+    expect(response.body).toHaveProperty("cpf");
+    expect(response.body).toHaveProperty("age");
+    expect(response.body).toHaveProperty("email");
+    expect(response.body).toHaveProperty("isAdm");
+    expect(response.body).toHaveProperty("isActive");
+    expect(response.body).not.toHaveProperty("password");
+    expect(response.body.name).toEqual("Juarez");
+    expect(response.body.email).toEqual("juarez@mail.com");
+    expect(response.body.cpf).toEqual("15865335683");
+    expect(response.body.isActive).toEqual(true);
+    expect(response.body.isAdm).toEqual(false);
+    expect(response.status).toBe(201);
   });
+
+  test("POST /profile - should not be able to create a user that already exists", async () => {
+    const response = await request(app).post("/profile").send(mockedUser);
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(400);
+  });
+
+  test("GET /profile - Must be able to list users", async () => {
+    await request(app).post("/users").send(mockedUserAdm);
+    const adminLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedLoginAdm);
+    const response = await request(app)
+      .get("/profile")
+      .set("Authorization", `Bearer ${adminLoginResponse.body.token}`);
+
+    expect(response.body).toHaveLength(2);
+  });
+
+  test("GET /profile - should not be able to list users without authentication", async () => {
+    const response = await request(app).get("/profile");
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(401);
+  });
+
+  test("GET /profile - should not be able to list users not being admin", async () => {
+    const userLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedLoginUser);
+    const response = await request(app)
+      .get("/profile")
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`);
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(403);
+  });
+
+  test("DELETE /profile - should not be able to delete user without authentication", async () => {});
 });
--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -1,4 +1,5 @@
 import { IAddressRequest } from "../../interfaces/address";
+import { ILogin } from "../../interfaces/login";
 import { IUserRequest } from "../../interfaces/users";
 
 export const mockedAddress: IAddressRequest = {
@@ -14,5 +15,24 @@ export const mockedUser: IUserRequest = {
   cpf: "15865335683",
   email: "juarez@mail.com",
   name: "Juarez",
+  password: "123456",
+};
+
+export const mockedLoginUser: ILogin = {
+  email: "juarez@mail.com",
+  password: "123456",
+};
+
+export const mockedUserAdm: IUserRequest = {
+  birthDate: "10/12/1996",
+  cpf: "15865335683",
+  email: "ademir@mail.com",
+  name: "Ademir",
+  password: "123456",
+  isAdm: true,
+};
+
+export const mockedLoginAdm: ILogin = {
+  email: "ademir@mail.com",
   password: "123456",
 };
