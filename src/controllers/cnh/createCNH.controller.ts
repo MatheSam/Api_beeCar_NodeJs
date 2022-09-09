6460a12c0be761f6786eaae80c9df8b64443155b
matheuszeiser <matheuszaizer@hotmail.com>
2022-09-09 17:16:06 -0300

    feat: Create CNH routes and fix a bugs

---
+++ src/controllers/cnh/createCNH.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { AppError } from "../../errors/AppError";
+import { handleError } from "../../middlewares/errors.mid";
+import createCNHService from "../../services/cnh/createCNH.service";
+
+const createCNHController = async (req: Request, res: Response) => {
+  try {
+    const cnh = await createCNHService(req.body, req.user.id);
+    return res.status(201).json(cnh);
+  } catch (error) {
+    if (error instanceof AppError) {
+      handleError(error, res);
+    }
+  }
+};
+
+export default createCNHController;
